6752bdefc4a79603efce79d11133f44bc8cb4866
Leelakanakala09 <ksleela09072007@gmail.com>
2026-01-31 11:25:09 +0530

    Add function to get nearest hospital details

---
+++ hospitals.py
@@ -0,0 +1,6 @@
+def get_nearest_hospital(emergency):
+    return {
+        "name": "Apollo Trauma & Emergency Care",
+        "distance": "2.3 km",
+        "maps": "https://www.google.com/maps/search/trauma+hospital+near+me"
+    }
